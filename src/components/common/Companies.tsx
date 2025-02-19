736bffc1fcbc328f8bdac304859454fc1a931058
Khayal Khudiyev <khkhudiyev@kibrit.tech>
2025-02-19 00:25:15 -0800

    Add new company logos to the Companies component

--- src/components/common/Companies.tsx
+++ src/components/common/Companies.tsx
@@ -2,24 +2,45 @@
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
 import Autoplay from "embla-carousel-autoplay"
 import Image from "next/image"
+import BelgianRailway from "../../../public/companies/belgianrailway.svg"
+import BirthdayParty from "../../../public/companies/birthday-party.svg"
+import Casa from "../../../public/companies/casa.svg"
 import Earma from "../../../public/companies/earma.svg"
+import Eca from "../../../public/companies/eca.svg"
 import Ellis from "../../../public/companies/ellis.svg"
 import Esae from "../../../public/companies/esae.svg"
 import EuroCommerce from "../../../public/companies/euro-commerce.svg"
-import KevinMurphy from "../../../public/companies/kevin-murphy.svg"
+import EuropeanGreens from "../../../public/companies/european-greens.svg"
+import Greenmood from "../../../public/companies/greemood.svg"
+import Hilton from "../../../public/companies/hilton.svg"
+import KevinMurphy from "../../../public/companies/kevin-murphy-K.svg"
+import Marriage from "../../../public/companies/marriage.svg"
+import MCI from "../../../public/companies/MCI.svg"
 import OliverWyman from "../../../public/companies/oliver-wyman.svg"
 import SolarPower from "../../../public/companies/solar-power.svg"
 import Una from "../../../public/companies/una.svg"
+import USEBrussel from "../../../public/companies/use-brussels.svg"
+
 
 const companyLogos = [
-	{ src: EuroCommerce, alt: "EuroCommerce" },
-	{ src: Esae, alt: "Esae" },
-	{ src: OliverWyman, alt: "Oliver Wyman" },
+	{ src: BelgianRailway, alt: "Belgian Railway" },
+	{ src: BirthdayParty, alt: "Birthday Party" },
+	{ src: Casa, alt: "Casa" },
 	{ src: Earma, alt: "Earma" },
+	{ src: Eca, alt: "Eca" },
+	{ src: Ellis, alt: "Ellis" },
+	{ src: Esae, alt: "Esae" },
+	{ src: EuroCommerce, alt: "EuroCommerce" },
+	{ src: EuropeanGreens, alt: "European Greens" },
+	{ src: Greenmood, alt: "Greenmood" },
+	{ src: Hilton, alt: "Hilton" },
+	{ src: KevinMurphy, alt: "Kevin Murphy" },
+	{ src: Marriage, alt: "Marriage" },
+	{ src: MCI, alt: "MCI" },
+	{ src: OliverWyman, alt: "Oliver Wyman" },
 	{ src: SolarPower, alt: "Solar Power" },
 	{ src: Una, alt: "Una" },
-	{ src: Ellis, alt: "Ellis" },
-	{ src: KevinMurphy, alt: "Kevin Murphy" },
+	{ src: USEBrussel, alt: "USE Brussel" },
 ]
 
 const Companies = () => {
